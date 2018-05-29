a48b924541e0bd96ed0f32b5abb6bc3d876f78ab
Jose Luis Rubio <joshsk8rocker@gmail.com>
2018-05-29 09:00:55 -0600

    Rama Control
    1. Cambios en el menú del administrador, tales como:
    - Registro de ronda.
    - Registro de pregunta.
    - Registro de grupos.

--- js/MenuAdministrador.js
+++ js/MenuAdministrador.js
@@ -1,6 +1,6 @@
-//Swap para que el administrador pueda armar un juego.
-$("#Inicio").click(function(){ $.post('../swap/registrarConcurso.html', {}, function(data){ $("#contenido").html(data);});});
-//Swap para que el administrador pueda registrar un juego.
-$("#registrarGrupo").click(function(){ $.post('../swap/registrarGrupo.html', {}, function(data){ $("#contenido").html(data);});});
-//Swap para que el administrador pueda cerrar sesión.
-//$("#Salir").click(function(){ $.post('../index.html', {}, function(data){ $("#contenido").html(data);});});
+//Swap para que el administrador pueda editar y eliminar preguntas, rondas y grupos
+$("#configurar").click(function(){ $.post('../SWAP/RegistrarRonda.html', {}, function(data){ $("#contenido").html(data);});});
+//Swap para que el administrador pueda registrar un grupo.
+$("#registrarGrupo").click(function(){ $.post('../SWAP/registrarGrupo.html', {}, function(data){ $("#contenido").html(data);});});
+//Swap para que el administrador pueda registrar un concurso.
+$("#registrarConcurso").click(function(){ $.post('../SWAP/registrarConcurso.html', {}, function(data){ $("#contenido").html(data);});});
